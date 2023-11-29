13765ec32ad75b0389efe25ad9d07428f3228a86
Vladyslav Lypivets <vladyslav.lypivets@tui.be>
2023-11-29 13:02:12 +0200

    add index bar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="page">
+    <IndexBar :index-list="categories" :sticky-offset-top="60">
+      <div class="container">
+        <section v-for="category in categories" :key="category" class="section">
+          <IndexAnchor :index="category">Title {{ category }}</IndexAnchor>
+          <Cell :title="text" />
+        </section>
+      </div>
+    </IndexBar>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { IndexBar, IndexAnchor, Cell } from 'vant';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const categories = [
+  "Populair ❤️",
+  "Menus & Chicken Mix",
+  "The BOX",
+  "Voorgerechten",
+  "Pan pizza medium",
+  "Classic pizza medium",
+  "Classic pizza large",
+  "Pan pizza large",
+  "Cheesy crust pizza large",
+  "Desserten",
+  "Dranken"
+];
+const text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corrupti culpa minus libero eligendi fugit nam, nostrum sapiente fuga consectetur aliquid quo voluptatem, necessitatibus quas! Animi iusto nihil tempore vitae sint neque quasi qui ullam quae. Assumenda rem quisquam dicta sunt! Provident quam enim, excepturi eum sequi saepe rerum repellendus!";
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.page {
+  padding-top: 60px;
+}
+
+.container {
+  width: 1200px;
+  margin: 0 auto;
+}
+
+.section {
+  margin-bottom: 50px;
+  font-size: 40px;
+}
+
+div.van-index-bar__sidebar {
+  padding: 20px 0;
+  top: 0;
+  flex-direction: row;
+  transform: translateY(0);
+  background: #fff;
+  box-shadow: 0 0 3px 2px rgba(0, 0, 0, 0.2);
+  width: 100%;
+  justify-content: center;
 }
 </style>
